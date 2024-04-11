5bdc53c7aab10c775d0455e283a1b0daf44ed091
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-04-11 21:21:45 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '002503e3-7073-4a1f-a39d-4957a2c4e146';
+export const MODAL_CLOSE_SEC = 2.5;
